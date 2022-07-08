bc9e9a5d3affb8b287d8107e8b83fa5112782512
Ranyaieditorm <108958998+Ranyaieditorm@users.noreply.github.com>
2022-07-09 01:23:45 +0300

    RANYAI_BOT_Riklam
    بۆتی ڕانیەیم تەیبەت بە ڕیکلام🤍

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const talkedRecently = new Set();
 
 
 client.on("ready", () => {
-  console.log("Vex ★ MrHAMA")
-  client.user.setActivity("Send Link Robot UP , MrHama")
+  console.log("RANYAI_BOT_Riklam")
+  client.user.setActivity("Send Link Robot UP , RANYAI")
 })
 
 
 
 client.on("message", vex => { 
-if(vex.content.includes("discord.gg")){ 
+if(vex.content.includes("https://discord.gg/J3B36PN7wU")){ 
     
     if (talkedRecently.has(vex.author.id)) {
-      vex.author.send("Wait 1 minute before getting typing this again. - ");
+      vex.author.send("1 خولەک چاوەڕێ بکە پێش ئەوەی جارێکی تر ئەمە چاپ بکەیتەوە. - ");
     } else {
 
 if(vex.channel.type === "dm"){ 
 if(vex.author.id===client.user.id) return; 
 vex.author.send("https://discord.gg/BmzN8ZvgQN")
 let s2 = vex .content = vex.content.replace(/@(everyone)/, "🥰").replace(/@(here)/, "🥰");
-let hama1 = client.channels.cache.get("755228220911255552")//id bashe reklam
+let hama1 = client.channels.cache.get("995081431925657682")//id bashe reklam
 hama1.send(`
 ناردرا لە لایەن
 <@${vex.author.id}> ` +`
@@ -41,13 +41,13 @@ ${s2}
 })
 
 const channelid = "848301704100577290"//id channele vc
-client.on("ready", () => {
+client.on("ready", (r!) => {
     const channel = client.channels.cache.get(channelid);
     if (!channel) return console.error("The channel does not exist!");
-    channel.join().then(connection => {
+    channel.join(995081431925657682).then(connection => {
         console.log("Successfully connected.");
-    }).catch(e => {
-        console.error(e);
+    }).catch(r! => {
+        console.error(r!);
     });
 });
 
